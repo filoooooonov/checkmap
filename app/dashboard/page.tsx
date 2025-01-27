4fb604aeeb765261148c8fc18cea8b7544634cb5
ElVarsov <ignaciowasowicz@gmail.com>
2025-01-27 18:04:43 +0200

    list of events

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,16 +1,18 @@
 "use client";
-
+import { useEffect, useState } from "react";
 import Image from "next/image";
 import React from "react";
+import Link from "next/link";
 import img from "@/public/placeholder-user.png";
 import { useSession } from "next-auth/react";
 import { redirect } from "next/navigation";
-
+import { getListOfEvents } from "@/actions/getListOfEvents";
 export default function Page() {
   // TODO: get user from session, redirect if the user is unauthenticated
 
   const { data: session, status } = useSession();
-
+  const [events, setEvents] = useState<any[]>([]);
+  const [loading, setLoading] = useState(true);
   if (status !== "authenticated" && status !== "loading") {
     redirect("/");
   }
@@ -18,7 +20,20 @@ export default function Page() {
   if (!session?.user?.id) {
     return <div className="text-center mt-80">Loading...</div>;
   }
+  useEffect(() => {
+    const fetchEvents = async () => {
+      try {
+        const data = await getListOfEvents(session.user.id);
+        if (data) setEvents(data);
+      } catch (error) {
+        console.error("Failed to fetch events:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
 
+    fetchEvents(); // Call the async function
+  });
   return (
     <div className="max-w-2xl mx-auto">
       {/* Account info */}
@@ -38,10 +53,18 @@ export default function Page() {
 
       {/* Event data */}
       <div className="p-4 pt-6">
-        <h2 className="text-xl font-bold">Your events</h2>
-        <p className="text-neutral-500 mt-40 text-center">
-          No events here yet...
-        </p>
+      <h2 className="text-xl font-bold">Your events</h2>
+        {events.length > 0 ? (
+          <ul>
+              {events.map((event) => (
+                <li key={event._id}><Link href={`/${event.eventCode}`}>{event.name}</Link></li>
+              ))}
+          </ul>
+        ) : (
+          <p className="text-neutral-500 mt-40 text-center">
+            You haven't created any events yet.
+          </p>
+        )}
       </div>
     </div>
   );
