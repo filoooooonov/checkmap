205c10edec923a0037332b2eb70c888be3dcd3c5
ElVarsov <ignaciowasowicz@gmail.com>
2025-01-29 17:29:56 +0200

    fix remove event

--- actions/removeEvent.ts
+++ actions/removeEvent.ts
@@ -9,7 +9,7 @@ export async function removeEvent(eventId: string) {
   await connectMongoDB();
 
   try {
-    const event = await Event.findOneAndDelete({ _id: eventId });
+    const event = await Event.findOne({ _id: eventId });
     if (event) {
       await Checkpoint.deleteMany({ eventId: eventId });
       const userUpdateResult = await User.findByIdAndUpdate(
@@ -17,10 +17,11 @@ export async function removeEvent(eventId: string) {
         { $pull: { events: event._id } }, // Remove the event ID from the `events` array
         { new: true } // Return the updated user document
       );
-
+    
       if (!userUpdateResult) {
         throw new Error("Failed to dissociate the event from the user");
       }
+      await Event.findOneAndDelete({ _id: eventId });
     } else {
       throw new Error("Event not found");
     }
