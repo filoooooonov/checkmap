dcf5698c5a0d36b5d0eb91a262198f49b0c6c89e
ElVarsov <ignaciowasowicz@gmail.com>
2025-01-31 17:27:07 +0200

    minor changes

--- app/demo_register/page.tsx
+++ app/demo_register/page.tsx
@@ -1,6 +1,10 @@
 export default function About() {
-    return <div className="w-full h-screen flex items-center justify-center flex-col gap-8">
-        <h1 className="text-4xl font-bold">:(</h1>
-        <p>In order to keep our database clean this option is disabled now</p>
+  return (
+    <div className="w-full h-screen flex items-center justify-center flex-col gap-8 p-4">
+      <h1 className="text-4xl font-bold text-center">:(</h1>
+      <p className="text-center">
+        In order to keep our database clean this option is disabled now
+      </p>
     </div>
-  }
+  );
+}
