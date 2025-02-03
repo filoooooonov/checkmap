e7c6dcc1c06f2fbe922d74c705a9f7d0adb8af39
Aleksei Filonov <alexfiloonov@gmail.com>
2025-02-03 09:48:45 +0200

    Add loading to /[eventCode]

---
+++ app/[eventCode]/loading.tsx
@@ -0,0 +1,12 @@
+import MapLoaderScreen from "@/components/MapLoaderScreen";
+import React from "react";
+
+const loading = () => {
+  return (
+    <div>
+      <MapLoaderScreen />
+    </div>
+  );
+};
+
+export default loading;
